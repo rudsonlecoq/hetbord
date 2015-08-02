737d56884f59de52408264b579bdba9a7782b8a3
rudsonlecoq <rutzor@gmail.com>
2015-08-02 21:07:13 +0200

    Add foundation

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'sqlite3'
 gem "bcrypt-ruby", :require => "bcrypt"
 
 gem 'protected_attributes'
+
+gem 'foundation-rails'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ def create
   user = User.authenticate(params[:username], params[:password])
   if user
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Logged in!"
+    redirect_to bord_path, :notice => "Logged in!"
   else
     flash.now.alert = "Invalid username or password"
     render "new"
